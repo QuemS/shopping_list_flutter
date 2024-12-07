0a7b4afbc721b31eb46169a439869fd41405d69c
Sushchenok Mykhailo <78119979+QuemS@users.noreply.github.com>
2024-12-07 16:44:16 +0200

    211. The Form & TextFormField Widgets

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addNewItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +24,12 @@ class GroceryList extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+            onPressed: _addNewItem,
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
