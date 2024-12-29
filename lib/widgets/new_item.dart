51e0cb9dcd7bdbea91090aea5e9e069bf44c4bd9
Sushchenok Mykhailo <78119979+QuemS@users.noreply.github.com>
2024-12-29 09:07:40 +0200

    227. Fetching & Transforming Data

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shopping_list_app/data/categories.dart';
 import 'package:shopping_list_app/models/category.dart';
-import 'package:shopping_list_app/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
